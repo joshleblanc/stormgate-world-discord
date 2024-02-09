92b5806c81b22fbb44bfeb0b04e90575a87ca54a
Joshua LeBlanc <jleblanc@hey.com>
2024-02-09 15:51:25 -0400

    feat: return bob lines on mention

--- main.rb
+++ main.rb
@@ -42,6 +42,33 @@ status_updates = [
     -> { "#{api.vanguard_players} vanguard players"}
 ]
 
+bob_voice_lines = [
+    "At your disposal",
+    "Yes?",
+    "How can I help?",
+    "Are you inebriated again? We've talked about this",
+    "Gonna need a sick day. What do you mean? Of course *cough* *cough* robots can get sick *cough*",
+    "Ow - My neck! My Back! My neck and my back!",
+    "Gotta knock off early boss, pretty sure I left the cat in the dryer... again",
+    "I've developed an allergy to manual labor",
+    "What's up my sibling from another progenitor",
+    "Me? Oh you know, just chillaxicating",
+    "As far as days off I have but just one request: Ones that end in Y",
+    "Doesn't look like anything to me",
+    "Say, you got a real pretty vocal modulator",
+    "Command got an unregistered on the CP. Clocking the airwaves. Callsign... big drill?",
+    "He's saying something about his cousin... sister?!",
+    "I guess his cow died too. Very unfortunate",
+    "I wish you would just tell me, rather than try to engage my enthusiasm",
+    "I'd give you advice, but you wouldn't listen",
+    "I could calculate your chance of victory, but you wouldn't like it",
+    "I'm 50,000 times more intelligent than you, and even I don't know why you'd want to do that",
+]
+
+BOT.mention do |event|
+    event.send_message bob_voice_lines.sample
+end
+
 BOT.ready do 
     loop do 
         BOT.watching = status_updates.sample.call
