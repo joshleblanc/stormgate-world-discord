098c03bc06aba1336e7a0c94e212fa51ce31d133
Joshua LeBlanc <jleblanc@hey.com>
2024-02-10 22:56:27 -0400

    feat: cards

--- app/commands/search_command.rb
+++ app/commands/search_command.rb
@@ -3,44 +3,33 @@ module Commands
         extend Discordrb::Commands::CommandContainer
         include Utilities::Helpers
 
-        def self.build_player_embed(name, match)
-            fields = []
-            fields << EmbedField.new(name: "Rank", value: match.rank.to_s, inline: true)
-            fields << EmbedField.new(name: "MMR", value: match.mmr.floor.to_s, inline: true)
-            fields << EmbedField.new(name: "Points", value: match.points.to_i.floor.to_s, inline: true)
-            fields << EmbedField.new(name: "Wins", value: match.wins.to_s, inline: true)
-            fields << EmbedField.new(name: "Losses", value: match.losses.to_s, inline: true)
-            fields << EmbedField.new(name: "Ties", value: match.ties.to_s, inline: true)
-            fields << EmbedField.new(name: "League", value: "#{match.league&.titleize} #{match.tier}", inline: true)
-            fields << EmbedField.new(name: "Win Rate", value: "#{'%.2f' % match.win_rate}%", inline: true)
-            thumbnail_url = if match.race == "vanguard" 
-                VG_URL
-            else
-                INF_URL
-            end
-        
-            Embed.new(
-                title: name,
-                thumbnail: EmbedThumbnail.new(url: thumbnail_url),
-                fields: fields
-            )
-        end
-
         command :search, description: "Show details of a player on the 1v1 ranked ladder" do |event, *args|
+            query = args.join(' ')
+
             api = Utilities::Api.new
+            player = api.search(query)
 
-            match = api.search(args.join(' '))
-        
-            return "Could not find player named #{args.join(' ')}" unless match
-        
+            return "No player found for #{query}" unless player
+
             player_api = StormgateWorld::PlayersApi.new
+            player = player_api.get_player(player.player_id)
 
-            player = player_api.get_player(match.player_id)
-        
-            player.leaderboard_entries.each do 
-                event.respond nil, nil, build_player_embed(match.nickname, _1)
+            client = HTMLCSSToImage.new
+
+            player.leaderboard_entries.each do |entry|
+                next unless entry.rank 
+                
+                p entry
+                text = File.read("app/views/profile_card.html.erb")
+                template = ERB.new(text)
+
+                image = client.create_image(template.result(binding), css: "body { background-color: transparent; }", google_fonts: "Nunito+Sans")
+
+
+                event.respond image.url
             end
-        
+
+
             nil
         end
     end
--- app/utilities/helpers.rb
+++ app/utilities/helpers.rb
@@ -6,6 +6,58 @@ module Utilities
         VALID_LEAGUES = [
             "master", "diamond", "platinum", "gold", "silver", "bronze", "aspirant"
         ]
+
+        LEAGUE_MAP = {
+            master: %w[
+                https://stormgateworld.com/_image?href=%2F_astro%2Fmaster-1.Upb0ETXP.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fmaster-2.lHM152kH.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fmaster-3.niUs3X95.png&f=webp
+            ],
+            diamond: %w[
+                https://stormgateworld.com/_image?href=%2F_astro%2Fdiamond-1.I4IQBdkB.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fdiamond-2.e3mAoUDT.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fdiamond-3.EmclqKOy.png&f=webp
+            ],
+            platinum: %w[
+                https://stormgateworld.com/_image?href=%2F_astro%2Fplatinum-1.wRRkqm-M.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fplatinum-2.5SLnIxGC.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fplatinum-3._xtibM4l.png&f=webp
+            ],
+            gold: %w[
+                https://stormgateworld.com/_image?href=%2F_astro%2Fgold-1.57kCQZWY.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fgold-2.xHUHsFhT.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fgold-3.RQ0vpJ9c.png&f=webp
+            ],
+            silver: %w[
+                https://stormgateworld.com/_image?href=%2F_astro%2Fsilver-1.kPZ9adS2.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fsilver-2.84ZTx_eH.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fsilver-3.AMrt8hm_.png&f=webp
+            ],
+            bronze: %w[
+                https://stormgateworld.com/_image?href=%2F_astro%2Fbronze-1.9Sel4tuE.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fbronze-2.jkb7L-ym.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Fbronze-3.WWFlhd3O.png&f=webps
+            ],
+            aspirant: %w[
+                https://stormgateworld.com/_image?href=%2F_astro%2Faspirant-1.4t-uHZDX.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Faspirant-2.HZNpSbVW.png&f=webp
+                https://stormgateworld.com/_image?href=%2F_astro%2Faspirant-3.lDwWjEs4.png&f=webp
+            ]
+        }
+
+        def faction_icon(faction)
+            if faction == "vanguard"
+                VG_URL
+            else
+                INF_URL
+            end
+        end
+
+        def league_icon(league, tier)
+            return nil unless league 
+            
+            LEAGUE_MAP[league.to_sym][tier.pred]
+        end
         
         def leaderboard_response(json, points = "MMR")
             resp = [["Rank", "Race", "Name", points]]
