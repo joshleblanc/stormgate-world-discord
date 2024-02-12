3d313acb857c21c4b8734c44ab1166c3dacaa6d4
Joshua.LeBlanc <jleblanc@omics.com>
2024-02-12 14:49:42 -0400

    fix: bronze 3 icon didn't render

--- app/utilities/helpers.rb
+++ app/utilities/helpers.rb
@@ -36,7 +36,7 @@ module Utilities
             bronze: %w[
                 https://stormgateworld.com/_image?href=%2F_astro%2Fbronze-1.9Sel4tuE.png&f=webp
                 https://stormgateworld.com/_image?href=%2F_astro%2Fbronze-2.jkb7L-ym.png&f=webp
-                https://stormgateworld.com/_image?href=%2F_astro%2Fbronze-3.WWFlhd3O.png&f=webps
+                https://stormgateworld.com/_image?href=%2F_astro%2Fbronze-3.WWFlhd3O.png&f=webp
             ],
             aspirant: %w[
                 https://stormgateworld.com/_image?href=%2F_astro%2Faspirant-1.4t-uHZDX.png&f=webp
