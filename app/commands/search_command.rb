acfba83104a6de156bc9e732e37d819d6aeb3ece
Joshua LeBlanc <jleblanc@hey.com>
2024-07-30 15:15:29 -0300

    feat: multi search

--- app/commands/search_command.rb
+++ app/commands/search_command.rb
@@ -7,29 +7,31 @@ module Commands
       event.channel.start_typing
 
       query = args.join(" ")
+      queries = query.split(",")
 
       api = Utilities::Api.new
-
-      player = api.find_player(query)
-
-      return "No player found for #{query}" if !player || player["detail"] == "Not found."
-
-      if player["ranks"]["ranked_1v1"].nil?
-        return "No ranks found for #{player["playerName"]}"
+      players = queries.map do
+        api.find_player(_1.strip)
       end
 
-      data = player["ranks"]["ranked_1v1"]
-
       json = []
-      data.each do |k, v|
-        v["race"] = k
-        v["playerName"] = player["playerName"]
-        json.push v
+
+      players.each do |player|
+        if player && player["ranks"]["ranked_1v1"].nil?
+          # event << "No ranks found for #{player["playerName"]}"
+        elsif player
+          data = player["ranks"]["ranked_1v1"]
+          data.each do |k, v|
+            v["race"] = k
+            v["playerName"] = player["playerName"]
+            json.push v
+          end
+        end
       end
 
       json.sort_by! { _1["points"].to_f }.reverse!
 
-      send_html event, render("leaderboard", { json:, description: player["playerName"] })
+      send_html event, render("leaderboard", { query: query, json:, description: json.map { _1["playerName"] }.uniq.join(", ") })
     end
   end
 end
